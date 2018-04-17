d901670ef674c1d05dc6c6a5f0059c25404283de
zky <1964483239@qq.com>
2018-04-17 08:23:33 +0800

    漏提的HelloDubboService接口。

---
+++ dubboProvider01/src/main/java/com/htt/dubbo/service/HelloDubboService.java
@@ -0,0 +1,13 @@
+package com.htt.dubbo.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: 19644
+ * Date: 2018-04-15 015
+ * Time: 00:59:24
+ * Description:
+ */
+public interface HelloDubboService
+{
+    public String sayHello(String name);
+}
